06b5e7b2f879290ce4017c721850cb63152b4494
akansha-singh <singhakansha281@gmail.com>
2020-05-12 01:03:19 +0530

    view-engine setup, path, body-parser middleware

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
 
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 app.get('/', (req,res) => {
-    res.send('Hello');
+    res.render('contact', {layout: false});
 });
 
 app.listen(7000, () => console.log('Server started at port 7000...'));
